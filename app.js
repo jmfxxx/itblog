9ef772cef50df700446f6bc3086e3750dc042557
jmfxxx <jmfxxx@gmail.com>
2020-10-13 01:22:16 +0800

    新增管理者帳號，測試用

--- app.js
+++ app.js
@@ -1,32 +1,30 @@
 // 引入express
-const express = require('express')
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const app = express();
 // 資料庫連接
-require('./model/connect')
-
+require("./model/connect");
 
 //開放靜態資源文件
-app.use(express.static(path.join(__dirname,'public')))
+app.use(express.static(path.join(__dirname, "public")));
 
 // 1.告訴express模板所在框在
-app.set('views',path.join(__dirname,'views'));
+app.set("views", path.join(__dirname, "views"));
 // 2.告訴 express模版預設的副檔名為何
-app.set('view engine','art');
+app.set("view engine", "art");
 // 3.當渲染為art的模板時，所使用的模板引掔是什麼
-app.engine('art',require('express-art-template'));
-
+app.engine("art", require("express-art-template"));
 
 // 導入route模組  .js可省略
-const home =require('./route/home')
-const admin = require('./route/admin')
+const home = require("./route/home");
+const admin = require("./route/admin");
 
 // 設定攔截請求 路由匹配路徑
-app.use('/home',home);
-app.use('/admin',admin)
+app.use("/home", home);
+app.use("/admin", admin);
 
 // list port
 app.listen(80);
 
-console.log('Server start !! http://localhost')
+console.log("Server start !! http://localhost");
--- model/user.js
+++ model/user.js
@@ -1,41 +1,52 @@
 // 建立使用者集合
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 // 創建用戶集合規則
-const userSchema = new mongoose.Schema ({
-    username:{
-        type:String,
-        required:true,
-        minlength:2,
-        maxlength:20
-    },
-    email:{
-        type:String,
-        // 保証email不重複
-        unique: true,
-
-    },
-    password:{
-        type:String,
-        useUnifiedTopology: true
-    },
-    role:{
-        type:String,
-        required:true
-    },
-    state:{
-        type:Number,
-        // 0啟用
-        // 1停用
-        default:0   
-    }
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 20,
+  },
+  email: {
+    type: String,
+    // 保証email不重複
+    unique: true,
+  },
+  password: {
+    type: String,
+    useUnifiedTopology: true,
+  },
+  //admin  , user
+  role: {
+    type: String,
+    required: true,
+  },
+  state: {
+    type: Number,
+    // 0啟用
+    // 1停用
+    default: 0,
+  },
 });
 
 //創建集合
-const User = mongoose.model('User',userSchema);
-
+const User = mongoose.model("User", userSchema);
+// User.create({
+//   username: "admin",
+//   email: "admin@don.local",
+//   password: "123456",
+//   role: "admin",
+//   state: 0,
+// })
+//   .then(() => {
+//     console.log("Create user success");
+//   })
+//   .catch((err) => {
+//     console.log("Error" + err);
+//   })
 // 將用集合做為模塊成員進行導出
-models.exports = {
-    User:User,
-
-} 
+module.exports = {
+  User: User,
+};
