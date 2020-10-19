cebd32a18c1fd69ab9989badb7e66094cdc9a640
chienxoo <chienxoo@gmail.com>
2020-10-19 15:44:10 +0800

    修正新增使用者後，會帶出undefine問題

--- route/admin/user-edit-fn.js
+++ route/admin/user-edit-fn.js
@@ -6,35 +6,35 @@ const bcrypt = require('bcrypt')
 const { object } = require('joi')
 
 
-module.exports =  async (req, res,next) => {
+module.exports = async (req, res, next) => {
 
 
 
     try {
 
-        const {error,value} = await validateUser(req.body)
+        const { error, value } = await validateUser(req.body)
         console.log('--------------------vaild')
-        
-        if (error ) {
+
+        if (error) {
             // JSON.stringfy() 將物件轉換字串
-       
+
             let errmsg = error
             let pp = {}
             pp.path = "/admin/user-edit"
-            pp.message =`${errmsg}`
+            pp.message = `${errmsg}`
 
 
             let cc = `{path:'/admin/user-edit,message=${error}}`
-            console.log('Iam cc : '+cc)
+            console.log('Iam cc : ' + cc)
             return next(JSON.stringify(pp))
-            
+
             // res.redirect(`/admin/user-edit?message=${error}`)
         }
-        res.redirect(`/admin/user-edit?message=${error}}`)
-       console.log(error)
-     
+        // res.redirect(`/admin/user-edit?message=${error}}`)
+        console.log(error)
+
     } catch (e) {
-     console.log('Er.... unknow error')
+        console.log('Er.... unknow error')
     }
     // 依據email信箱來判斷是否已註冊過了...
     // 使用findOne的原因他會回傳一個物件，find會返回一個陣列
@@ -45,7 +45,7 @@ module.exports =  async (req, res,next) => {
         // res.redirect 會執行一個res.end會造成後面送res.send錯誤
         // 解決方式加 return
         // return res.redirect(`/admin/user-edit?message=email已被註冊過`)
-        return next(JSON.stringify({path:'/admin/user-edit',message:'email已被註冊過'}))
+        return next(JSON.stringify({ path: '/admin/user-edit', message: 'email已被註冊過' }))
     }
 
     //對密碼進行加密處理 bcrypt
@@ -59,5 +59,5 @@ module.exports =  async (req, res,next) => {
     //將新增使用者資料加入資料庫
     await User.create(req.body);
     //  將網頁重定向回到user頁面
-    res.redirect('/admin/user')
+    res.redirect('/admin/user?message=""')
 }
